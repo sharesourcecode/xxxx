d6bd20b2f4a7b802511dbcc883e4ae9754d3cc00
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-09-27 19:03:46 -0300

    Add files via upload

--- requeriments.sh
+++ requeriments.sh
@@ -90,7 +90,7 @@ _requeriments () {
 	if [[ $? = 0 ]] ; then
 		[[ ! -e executed.txt ]] && pkg install termux-api w3m curl dos2unix -y && >executed.txt
 # _sync - to disable coment #
-		[[ $(date +%H) -lt 10 || $(date +%H) -gt 22 ]] && _sync
+#		[[ $(date +%H) -lt 10 || $(date +%H) -gt 22 ]] && _sync
 		reset; clear
 		echo -e "Successful updates!\n"
 	else
@@ -101,8 +101,9 @@ _requeriments () {
 # /user agents to $TMP/.ua
 _userAgent () {
 	cd $TMP
+	echo -e 'Mozilla/5.0 (Linux; Android 7.1.2; Moto G Build/NJH47F; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/69.0.3497.109 Mobile Safari/537.36' >.ua
 #	echo -e 'Mozilla/5.0 (Android 7.1.2; Mobile; rv:80.0) Gecko/80.0 Firefox/80.0' >.ua
-	echo -e '"Mozilla/5.0 (BB10; Touch) AppleWebKit/537.35+ (KHTML, like Gecko) Version/10.3.2.2339 Mobile Safari/537.35+"\n"Mozilla/5.0 (Linux; U; Android 4.3; en-us; ZTE-Z667G Build/JLS36C) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"\n"Mozilla/5.0 (Mobile; rv:32.0) Gecko/32.0 Firefox/32.0"\n"Mozilla/5.0 (Android; Linux armv7l; rv:5.0) Gecko/20110615 Firefox/5.0 Fennec/5.0"' >.ua
+#	echo -e '"Mozilla/5.0 (BB10; Touch) AppleWebKit/537.35+ (KHTML, like Gecko) Version/10.3.2.2339 Mobile Safari/537.35+"\n"Mozilla/5.0 (Linux; U; Android 4.3; en-us; ZTE-Z667G Build/JLS36C) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"\n"Mozilla/5.0 (Mobile; rv:32.0) Gecko/32.0 Firefox/32.0"\n"Mozilla/5.0 (Android; Linux armv7l; rv:5.0) Gecko/20110615 Firefox/5.0 Fennec/5.0"' >.ua
 	clear
 	echo "Simulate your real or random device."
 	echo -e "\n1) Mannualy (Recommended)\n2) Automatic (not secure)\n"
